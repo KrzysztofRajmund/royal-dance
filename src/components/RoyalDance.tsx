59428b5738fade19ca09aeaf7aba2b2da0a30b00
Krzysztof Nadlonek <knadlonek@gmail.com>
2021-03-16 16:28:13 +0100

    first commit - typescript set up

---
+++ src/components/RoyalDance.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+//router
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+function RoyalDance() {
+  return (
+  <Router>
+    <div>
+      Hello
+    </div>
+  </Router>
+  );
+}
+
+export default RoyalDance;
