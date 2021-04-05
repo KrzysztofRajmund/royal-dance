3a31bbffd46d3a47744b09f189e9472c25691469
Krzysztof Nadlonek <knadlonek@gmail.com>
2021-04-05 14:40:07 +0200

    finished footer

--- src/components/Cards/CardsSlider.tsx
+++ src/components/Cards/CardsSlider.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import img1 from "../../assets/img1.jpg";
+import img1 from "../../assets/img1.jpg";
 // import img2 from "../../assets/img2.jpg";
 // import img3 from "../../assets/img3.jpg";
 
@@ -9,26 +9,26 @@ const CardsSlider: React.FC = () => {
 
 
     //array of object TYPESCRIPT: solution 1!
-    // const images: { id: number, title: string, url: string }[] = [
-    //     {
-    //         "id": 1,
-    //         "title": "",
-    //         "url": img1
+    const images: { id: number, title: string, url: string }[] = [
+        {
+            "id": 1,
+            "title": "",
+            "url": img1
 
-    //     },
-    //     // {
-    //     //     "id": 2,
-    //     //     "title": "",
-    //     //     "url": img2
+        },
+        // {
+        //     "id": 2,
+        //     "title": "",
+        //     "url": img2
 
-    //     // },
-    //     // {
-    //     //     "id": 3,
-    //     //     "title": "",
-    //     //     "url": img3
+        // },
+        // {
+        //     "id": 3,
+        //     "title": "",
+        //     "url": img3
 
-    //     // }
-    // ]
+        // }
+    ]
 
 
     //array of object TYPESCRIPT: solution 2!
@@ -61,11 +61,11 @@ const CardsSlider: React.FC = () => {
 
     return (
         <React.Fragment>
-            {/* {images.map((image) => {
+            {images.map((image) => {
                 return (
-                    <div><img src={image.url} alt="" /></div>
+                    <img key={image.id} src={image.url} alt="" />
                 )
-            })} */}
+            })}
         </React.Fragment>
     )
 }
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
-import Logo from "../../assets/logo.png"
+import Logo2 from "../../assets/logo2.png"
 
 const Footer: React.FC = () => {
+
+    const handleUpClick = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    }
+
     return (
         <section className="footer">
             <article className="footer-top">
-                <img src={Logo} alt="logo" />
+                <img src={Logo2} alt="logo" />
             </article>
-            <article className="ooter-middle">
+            <article className="footer-middle">
                 <h5>Copyrights 2021. Created by RajmundTech</h5>
             </article>
-            <article className="footer-bottom">
-                <img src="arrow" alt="up-image" />
+            <article className="footer-bottom" onClick={handleUpClick}>
+                <div className="footer-bottom-arrow"></div>
                 <h4>up</h4>
             </article>
         </section>
