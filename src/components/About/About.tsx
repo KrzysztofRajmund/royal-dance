069e973dc96b6dff66c2a60dcddc20b2da02f5ee
Krzysztof Nadlonek <knadlonek@gmail.com>
2021-05-21 17:28:27 +0200

    slight changes

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 //assets
 import Logo2 from "../../assets/logo2.png"
+//react router
+import { Link } from "react-router-dom"
 
 
 
 const About: React.FC = () => {
+
+    const scrollUp = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    }
+
+    let userLocation: any = window.location.pathname
+
 
     return (
 
@@ -21,17 +33,21 @@ const About: React.FC = () => {
                     <br />
 
                     <p>Royal Dance to szkoła tańca, która dopiero zaczyna swoją działalność, ale z bardzo doświadczoną kadrą trenerską. Założycielami są Magdalena Geldner i Adrian Kopczyński, oboje młodzi , doświadczeni i z ogromną pasją do tańca. Magdalena Geldner jest Akademicką Mistrzynią Polski w tańcach latynoamerykańskich, finalistką London Open " The 2 Richards" , wielokrotna zwyciężczyni i finalistka polskich i międzynarodowych turniejów. Posiada najwyższą klasę międzynarodową S.</p>
-
                     <p>Adrian Kopczyński wielokrotny mistrz Polski w tańcach latynoamerykańskich , vicemistrz Mistrzostw Świata w latino i vicemistrz Świata w 10 tańcach, finalista Blackpool Dance Festiwal , posiada najwyższą klasę międzynarodową S.</p>
-                    <p>Szkoleniowcami są : Andrej Mosejcuk i Kamila Kajak trzykrotni Mistrzowie par zawodowych, półfinaliści Mistrzostw Świata Europy oraz półfinaliści Blackpool.Marek Kosaty i Paulina Glazik trzykrotni Mistrzowie Swiata, dwukrotni zwycięzcy Blackpool Festival UK i International Open w Royal Albert Hall.Monika Marszałek tancerka baletu , która pochodzi z Krakowa gdzie uczyła się i pracowała w Operze Krakowskiej , obecnie związana z Operą na Zamku w Szczecinie.
+                    {userLocation === "/" ? <p><Link className="about-link" onClick={scrollUp} to="/onas" >czytaj więcej . . .</Link></p> : ""}
+                    {userLocation === "/onas" ? <>
+                        <p>Szkoleniowcami są : Andrej Mosejcuk i Kamila Kajak trzykrotni Mistrzowie par zawodowych, półfinaliści Mistrzostw Świata Europy oraz półfinaliści Blackpool.Marek Kosaty i Paulina Glazik trzykrotni Mistrzowie Swiata, dwukrotni zwycięzcy Blackpool Festival UK i International Open w Royal Albert Hall.Monika Marszałek tancerka baletu , która pochodzi z Krakowa gdzie uczyła się i pracowała w Operze Krakowskiej , obecnie związana z Operą na Zamku w Szczecinie.
                     Agata Korzemiacka trenerka przygotowania motorycznego oraz entuzjastka holistycznego podejścia do treningu. Autorka wielu programów dydaktycznych. </p>
-                    <p>Szkoleniowcami są : Andrej Mosejcuk i Kamila Kajak trzykrotni Mistrzowie par zawodowych, półfinaliści Mistrzostw Świata Europy oraz półfinaliści Blackpool.</p>
+                        <p>Szkoleniowcami są : Andrej Mosejcuk i Kamila Kajak trzykrotni Mistrzowie par zawodowych, półfinaliści Mistrzostw Świata Europy oraz półfinaliści Blackpool.</p></> : ""
+                    }
+
+
 
 
 
                 </div>
-            </article>
-        </section>
+            </article >
+        </section >
 
     )
 }
