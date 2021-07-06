7d56e63d9890042f9140b4a992798baafabb169e
Krzysztof Nadlonek <knadlonek@gmail.com>
2021-07-06 16:22:43 +0200

    changes for about content

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -31,15 +31,15 @@ const About: React.FC = () => {
                 <div className="cards-subcontent">
                     <p className="cards-subcontent-title">Szczecińska Szkoła Tańca Royal Dance to miejsce pełne pasji...</p>
                     <br />
-
-                    <p>Royal Dance to szkoła tańca, która dopiero zaczyna swoją działalność, ale z bardzo doświadczoną kadrą trenerską. Założycielami są Magdalena Geldner i Adrian Kopczyński, oboje młodzi , doświadczeni i z ogromną pasją do tańca. Magdalena Geldner jest Akademicką Mistrzynią Polski w tańcach latynoamerykańskich, finalistką London Open " The 2 Richards" , wielokrotna zwyciężczyni i finalistka polskich i międzynarodowych turniejów. Posiada najwyższą klasę międzynarodową S.</p>
-                    <p>Adrian Kopczyński wielokrotny mistrz Polski w tańcach latynoamerykańskich , vicemistrz Mistrzostw Świata w latino i vicemistrz Świata w 10 tańcach, finalista Blackpool Dance Festiwal , posiada najwyższą klasę międzynarodową S.</p>
-                    {userLocation === "/" ? <p><Link className="about-link" onClick={scrollUp} to="/onas" >czytaj więcej . . .</Link></p> : ""}
+                    <p>Szczecińska Szkoła Tańca Royal Dance to miejsce pełne pasji, gdzie każdy znajdzie coś dla siebie.</p>
+                    <p>Doświadczona kadra, fantastyczna atmosfera, klimatyczna sala oraz pełen profesjonalizm - to wszystko czego możecie doświadczyć przekraczając nasze progi.</p>
+                    <p>Pozwól Nam porwać Cię w świat tańca a obiecujemy, że będziesz kroczyć przez życi tanecznym krokiem.</p>
+                    {/* {userLocation === "/" ? <p><Link className="about-link" onClick={scrollUp} to="/onas" >czytaj więcej . . .</Link></p> : ""}
                     {userLocation === "/onas" ? <>
                         <p>Szkoleniowcami są : Andrej Mosejcuk i Kamila Kajak trzykrotni Mistrzowie par zawodowych, półfinaliści Mistrzostw Świata Europy oraz półfinaliści Blackpool.Marek Kosaty i Paulina Glazik trzykrotni Mistrzowie Swiata, dwukrotni zwycięzcy Blackpool Festival UK i International Open w Royal Albert Hall.Monika Marszałek tancerka baletu , która pochodzi z Krakowa gdzie uczyła się i pracowała w Operze Krakowskiej , obecnie związana z Operą na Zamku w Szczecinie.
                     Agata Korzemiacka trenerka przygotowania motorycznego oraz entuzjastka holistycznego podejścia do treningu. Autorka wielu programów dydaktycznych. </p>
                         <p>Szkoleniowcami są : Andrej Mosejcuk i Kamila Kajak trzykrotni Mistrzowie par zawodowych, półfinaliści Mistrzostw Świata Europy oraz półfinaliści Blackpool.</p></> : ""
-                    }
+                    } */}
 
 
 
