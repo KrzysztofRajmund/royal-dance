44e0e646b2ca43f7ebe046cb9665a1a97af4442e
Krzysztof Nadlonek <knadlonek@gmail.com>
2021-07-27 18:11:13 +0200

    card content changed

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -24,18 +24,15 @@ const Cards: React.FC = () => {
         <section className="cards-container">
             <article>
                 <div className="cards-content">
-                    <h1>Taniec towarzyski</h1>
-                    <h1>Main Content</h1>
+                    <h1>Kursy grupowe</h1>
+                    <h1>Kursy indywiduane</h1>
 
                 </div>
                 <div className="cards-subcontent">
-                    <p className="cards-subcontent-title">Chcesz wykorzystać go na dobrą rozrywkę?</p>
-                    <br />
-
                     <p>Masz trochę wolnego czasu i chcesz wykorzystać go na dobrą rozrywkę?</p>
                     <br />
 
-                    <p>Masz trochę wolnego czasu i chcesz wykorzystać go na dobrą rozrywkę?</p>
+                    <p>Zapraszamy do naszej szkoły tańca</p>
 
                 </div>
                 <div className="cards-slider">
